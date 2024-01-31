3f4e8fe573e7f97eb47fe39e3c2d6805adbdbf2b
Jair Torezone <jairtorezone@gmail.com>
2024-01-30 22:39:14 -0300

    CardInfo

--- src/components/CardInfo/index.tsx
+++ src/components/CardInfo/index.tsx
@@ -3,14 +3,25 @@ import './styles.css';
 function CardInfo() {
   return (
     <div className="info-container">
-      <div className="card-image"></div>
+      <div className="card-image">
+        <img src="https://github.com/jairTorezone.png" alt="profile" />
+      </div>
       <div className="card-info">
-        <h3>Informações</h3>
+        <h3>Informações </h3>
         <ul>
-          <li>Perfil: https://api.github.com/users/acenelio</li>
-          <li>Seguidores: 4379</li>
-          <li>Localidade: Uberlândia</li>
-          <li>Nome: Nelio Alves</li>
+          <li>
+            <span>Perfil: </span>
+            <a href="/">https://api.github.com/users/acenelio</a>
+          </li>
+          <li>
+            <span>Seguidores:</span> 43791
+          </li>
+          <li>
+            <span>Localidade:</span> Uberlândia
+          </li>
+          <li>
+            <span>Nome:</span> Nelio Alves
+          </li>
         </ul>
       </div>
     </div>
